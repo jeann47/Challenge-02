aa2da958ec5a241bfea7a00d5c39af3020021415
Jeann Carlos Batemarque <jeann.carlos4@gmail.com>
2020-04-12 11:54:52 -0300

    fix repositories array

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const repositories = [1,2,3];
+const repositories = [];
 
 const findById = (req, res, next) => {
   const {id} = req.params
